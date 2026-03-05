505606304d453148ca6d96252c2e074b399ce078
Felipe Cantu <cantujr.felipe@gmail.com>
2026-03-04 23:35:33 -0600

    cloud settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,9 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
